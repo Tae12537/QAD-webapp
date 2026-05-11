e8062742a810990c1842cfadd9b32360bc85ae86
Tae12537 <taevangtanapat@gmail.com>
2026-05-11 15:37:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def go_to_menu():
 # ==========================================
 def app_file_validator():
     st.markdown("<h1 class='center-text' style='color: #1e3a8a;'>📁 File Validator</h1>", unsafe_allow_html=True)
-    st.markdown("<p class='center-text' style='color: #64748b; font-size: 20px;'>ตรวจสอบ format ไฟล์ก่อนส่ง ✨</p>", unsafe_allow_html=True)
+    st.markdown("<p class='center-text' style='color: #64748b; font-size: 20px;'>ตรวจสอบ format ไฟล์ QPM ก่อนส่ง ✨</p>", unsafe_allow_html=True)
     
     # ดึง reset_counter มาใช้ควบคุม widget
     if 'reset_counter' not in st.session_state:
